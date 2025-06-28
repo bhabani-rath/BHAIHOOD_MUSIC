2bdc8eec4e3d70dc66298143cabcc6a3414159b3
Bhabani Shankar Rath <bhabanishankarr21@gmail.com>
2025-06-29 02:30:13 +0530

    Update entry point and reformat config.js
    Changed the main entry point in package.json from src/index.js to index.js and updated the start script accordingly. Reformatted src/config/config.js for consistent indentation and style.

--- src/config/config.js
+++ src/config/config.js
@@ -1,37 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 const { YtDlpPlugin } = require("@distube/yt-dlp");
 
 module.exports = {
-    token: process.env.TOKEN,  // from .env
+  token: process.env.TOKEN, // from .env
 
-    cookies: {
-        domain: ".youtube.com",
-        expirationDate: 1757764090.235338,
-        hostOnly: false,
-        httpOnly: false,
-        name: "__Secure-1PAPISID",
-        path: "/",
-        sameSite: "unspecified",
-        secure: true,
-        session: false,
-        storeId: "0",
-        value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
-        id: 1
-    },
+  cookies: {
+    domain: ".youtube.com",
+    expirationDate: 1757764090.235338,
+    hostOnly: false,
+    httpOnly: false,
+    name: "__Secure-1PAPISID",
+    path: "/",
+    sameSite: "unspecified",
+    secure: true,
+    session: false,
+    storeId: "0",
+    value: "1WEHRrYUBttCn9ih/ACbOwdBzceu9lwe-x",
+    id: 1,
+  },
 
-    distubeOptions: {
-        emitAddListWhenCreatingQueue: true,
-        emitAddSongWhenCreatingQueue: false,
-        emitNewSongOnly: true,
-        joinNewVoiceChannel: true,
-        nsfw: true,
-        savePreviousSongs: true,
+  distubeOptions: {
+    emitAddListWhenCreatingQueue: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitNewSongOnly: true,
+    joinNewVoiceChannel: true,
+    nsfw: true,
+    savePreviousSongs: true,
 
-        plugins: [
-            new YtDlpPlugin({
-                update: true, // auto-updates yt-dlp
-                cookies: "./src/config/cookies.txt" // Optional: remove if not using cookies
-            })
-        ]
-    }
+    plugins: [
+      new YtDlpPlugin({
+        update: true, // auto-updates yt-dlp
+        cookies: "./src/config/cookies.txt", // Optional: remove if not using cookies
+      }),
+    ],
+  },
 };
